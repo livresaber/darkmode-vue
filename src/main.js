c85c7146c22d309d8eae1171a64714857e2e375c
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-10-27 19:42:53 -0300

    remove pinia import

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import App from './App.vue'
 
 import './assets/main.css'
 
-const app = createApp(App).use(createPinia()).mount('#app')
+const app = createApp(App).mount('#app')
