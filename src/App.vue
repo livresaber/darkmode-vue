8c013b08642ff57ae230f4c5348aa31994f10863
lucasferreiralimax <lucasferreiralimax@gmail.com>
2023-04-28 23:44:25 -0300

    update for version 0.2.8

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import pkg from '../package.json';
-import DarkModeVue from '../dist/darkmode-vue';
+import DarkModeVue from 'darkmode-vue';
 import CodeComponent from './components/CodeComponent.vue';
 
 const appVersion = pkg.version;
