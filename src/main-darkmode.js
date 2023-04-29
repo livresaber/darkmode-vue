1e73675e6ee084175c7447692c34731361fbc7a7
lucasferreiralimax <lucasferreiralimax@gmail.com>
2023-04-29 17:29:19 -0300

    export useDarkModeVue with toggleMode and mode

--- src/main-darkmode.js
+++ src/main-darkmode.js
@@ -1,3 +1,9 @@
+import { toggleMode, mode } from './store'
 import DarkModeVue from './components/DarkMode.vue'
+
+export const useDarkModeVue = () => ({
+  toggleMode,
+  mode
+})
 
 export default DarkModeVue
