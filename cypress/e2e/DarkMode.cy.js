2846265dfe2fda8f8430379365728f98c3555282
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-05-19 19:21:40 -0300

    test(e2e): improvement spec of test darkmode

--- cypress/e2e/DarkMode.cy.js
+++ cypress/e2e/DarkMode.cy.js
@@ -1,14 +1,38 @@
-// https://docs.cypress.io/api/introduction/api.html
-
 describe('Test DarkMode', () => {
-  it('test light mode', () => {
-    cy.visit('/')
-    cy.contains('LightMode')
-    cy.get('[aria-label="Icon LightMode"]').should('exist')
-  })
-  it('test dark mode', () => {
-    cy.get('.btn-mode').click()
-    cy.contains('DarkMode')
-    cy.get('[aria-label="Icon DarkMode"]').should('exist')
-  })
-})
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('should start in light mode', () => {
+    cy.contains('LightMode');
+    cy.get('[aria-label="Icon LightMode"]').should('exist');
+    cy.get('body').should('not.have.class', 'darkmode');
+  });
+
+  it('should switch to dark mode', () => {
+    cy.get('.btn-mode').first().click();
+    cy.contains('DarkMode');
+    cy.get('[aria-label="Icon DarkMode"]').should('exist');
+    cy.get('body').should('have.class', 'darkmode');
+    cy.window().then((win) => {
+      expect(JSON.parse(win.localStorage.getItem('darkmode'))).to.be.true;
+    });
+  });
+
+  it('should switch back to light mode', () => {
+    cy.get('.btn-mode').first().click();
+    cy.get('.btn-mode').first().click(); // Switch to Dark Mode
+    cy.contains('DarkMode');
+    cy.get('[aria-label="Icon DarkMode"]').should('exist');
+    cy.get('body').should('have.class', 'darkmode');
+
+    // Switch back to Light Mode
+    cy.get('.btn-mode').first().click();
+    cy.contains('LightMode');
+    cy.get('[aria-label="Icon LightMode"]').should('exist');
+    cy.get('body').should('not.have.class', 'darkmode');
+    cy.window().then((win) => {
+      expect(JSON.parse(win.localStorage.getItem('darkmode'))).to.be.false;
+    });
+  });
+});
